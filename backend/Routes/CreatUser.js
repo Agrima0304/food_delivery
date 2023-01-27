3baa16148ff7b58122b83fb60f818d1af3677cc0
Asutosh Pradhan <75766416+Maagztech@users.noreply.github.com>
2023-01-27 20:47:08 +0530

    solved

--- backend/Routes/CreatUser.js
+++ backend/Routes/CreatUser.js
@@ -1,23 +1,25 @@
 const express = require('express')
-const router = express.Router()
-const User = require('../models/User')
+const router=express.Router()
+const User=require('../models/User')
 
-router.post("/creatuser", async (req, res) => {
-  try {
-    await User.create({
-      name: "Agrima Bansal",
-      password: "123456",
-      email: "agrima0304@gmail.com",
-      location: "lucknow"
-    })
-    res.json(
-      { success: true }
-    )
-  }
-  catch (error) {
+router.post("/creatuser",async(req,res)=>{
+   try{
+    const new_user= new User({ 
+       name:"Agrima Bansal",
+       password:"123456",
+       email:"agrima0304@gmail.com",
+       location:"lucknow"
+     });
+   await new_user.save();
+     res.json(
+       {success:true}
+       )
+   }
+   catch(error)
+   {
     console.log(error)
-    res.json({ success: false })
-  }
-})
+    res.json({success:false})
+   }
+ }) 
 
-module.exports = router;
+ module.exports=router;
--- backend/db.js
+++ backend/db.js
@@ -1,25 +1,10 @@
 const mongoose = require('mongoose');
-const mongoURI ='mongodb+srv://goFood:Agrima123@cluster0.y2crqso.mongodb.net/gofoodmern?retryWrites=true&w=majority'
-const mongoDB=async()=>{
-    await mongoose.connect(mongoURI,{ useNewUrlParser :true },async(err,result)=>{
-        if(err) {
-            console.log("---",err)
-        }
-        else{
-            console.log("connected");
-            const fetched_data= await mongoose.connection.db.collection("food_items");
-            fetched_data.find({}).toArray(function(err,data){
-                if(err) 
-                {
-                    console.log(err);
-                }
-                else 
-                {
-                    console.log(data);
-                }
-            })
-        }
-    });
-}
+const URI = 'mongodb+srv://goFood:Agrima123@cluster0.y2crqso.mongodb.net/gofoodmern?retryWrites=true&w=majority'
+mongoose.connect(`${URI}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, (err) => {
+    if (err) throw err;
+    console.log('Mongodb connection')
 
-module.exports=mongoDB;
+})
