3a12d7c36e7156d55c8d129e4de715c7da0310bd
agrima <agrima0304@gmail.com>
2023-08-11 19:07:36 +0530

    changes made in home , db,index and displaydata

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,30 @@
 const mongoose = require('mongoose');
-const URI = 'mongodb+srv://goFood:Agrima123@cluster0.y2crqso.mongodb.net/gofoodmern?retryWrites=true&w=majority'
+const URI = 'mongodb+srv://goFood:Agrima123@cluster0.y2crqso.mongodb.net/gofoodmern?retryWrites=true&w=majority';
+
 mongoose.connect(`${URI}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-}, (err) => {
-    if (err) throw err;
-    console.log('Mongodb connection')
-
 })
+.then(() => {
+    console.log('MongoDB connected');
+
+    const db = mongoose.connection.db;
+
+    // Fetch food items data
+    const fetchedDataPromise = db.collection("food_items").find({}).toArray();
+
+    // Fetch food category data
+    const foodCategoryPromise = db.collection("food_category").find({}).toArray();
+
+    return Promise.all([fetchedDataPromise, foodCategoryPromise]);
+})
+.then(([data, catData]) => {
+    global.food_items = data;
+    global.food_category = catData;
+    console.log('Data fetched and stored.');
+})
+.catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+});
+
+module.exports = mongoose.connection;
